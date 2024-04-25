8df69e67599555ba2c29046ba7453892900ff667
Kharafi03 <kharaffidwiandhika@gmail.com>
2024-04-25 11:07:40 +0700

    Merge 1

--- index.php
+++ index.php
@@ -3,3 +3,18 @@
     echo "Hello World";
     echo "Hello World";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Coba1</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dolores!
+        </p>
+    </body>
+</html>
