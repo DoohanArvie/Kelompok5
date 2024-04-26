fe41ac765996d691fd9fdc3d83a01f4abaf56a47
niddaaukar <111202113495@mhs.dinus.ac.id>
2024-04-26 08:56:25 +0700

    commit merge

---
+++ merge.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mrge -Nida Aulia Karima</title>
+</head>
+
+<body>
+    <h1>Latihan Merge</h1>
+    <p>Ini adalah merge pertama saya</p>
+</body>
+
+</html>
