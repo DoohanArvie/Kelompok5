938764f6cee4f9b1c3e23f1549003548519804d7
Kharafi03 <kharaffidwiandhika@gmail.com>
2024-05-22 14:15:46 +0700

    Team

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -5,11 +5,11 @@
 <div class="container-fluid header bg-white p-0">
     <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
         <div class="col-md-6 p-5 mt-lg-5">
-            <h1 class="display-5 animated fadeIn mb-4">Tentang Kami</h1> 
+            <h1 class="display-5 animated fadeIn mb-4">Kontak Kami</h1> 
                 <nav aria-label="breadcrumb animated fadeIn">
                 <ol class="breadcrumb text-uppercase">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item text-body active" aria-current="page">Tentang Kami</li>
+                    <li class="breadcrumb-item text-body active" aria-current="page">Kontak Kami</li>
                 </ol>
             </nav>
         </div>
