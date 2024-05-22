d5f9f0807a83f6e8e36ade6b0595e40de08107f6
Kharafi03 <kharaffidwiandhika@gmail.com>
2024-05-22 13:49:38 +0700

    Penamaan Gambar

--- resources/views/frontend/car/index.blade.php
+++ resources/views/frontend/car/index.blade.php
@@ -1,78 +1,6 @@
 @extends('frontend.layout')
 
 @section('content')
-<<<<<<< HEAD
-<div
-        class="hero inner-page"
-        style="background-image: url('{{ asset('frontend/images/hero_1_a.jpg') }}')"
-      >
-        <div class="container">
-          <div class="row align-items-end">
-            <div class="col-lg-5">
-              <div class="intro">
-                <h1><strong>Daftar Mobil</strong></h1>
-                <div class="custom-breadcrumbs">
-                  <a href="index.html">Home</a> <span class="mx-2">/</span>
-                  <strong>Daftar Mobil</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="site-section bg-light">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-7">
-              <h2 class="section-heading"><strong>Daftar Mobil</strong></h2>
-              <p class="mb-5">
-                Kami menyediakan berbagai macam mobil.
-              </p>
-            </div>
-          </div>
-
-          <div class="row">
-            @forelse($cars as $car)
-              <div class="col-md-6 col-lg-4 mb-4">
-                  <div class="listing d-block align-items-stretch">
-                    <div class="listing-img h-100 mr-4">
-                      <img src="{{ Storage::url($car->image) }}" alt="Image" class="img-fluid" />
-                    </div>
-                    <div class="listing-contents h-100">
-                      <h3>{{ $car->nama_mobil }}</h3>
-                      <div class="rent-price">
-                        <strong>Rp{{ number_format($car->price,0,",",".") }}</strong><span class="mx-1">/</span>hari
-                      </div>
-                      <div class="d-block d-md-flex mb-3 border-bottom pb-3">
-                        <div class="listing-feature pr-4">
-                          <span class="caption">Pintu:</span>
-                          <span class="number">{{ $car->pintu }}</span>
-                        </div>
-                        <div class="listing-feature pr-4">
-                          <span class="caption">Penumpang:</span>
-                          <span class="number">{{ $car->penumpang }}</span>
-                        </div>
-                      </div>
-                      <div>
-                        <p>
-                        {{ $car->description }}
-                        </p>
-                        <p>
-                          <a href="{{ route('car.show', $car) }}" class="btn btn-primary btn-sm">Sewa Sekarang</a>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            @empty
-                <p class="display-4 text-center mx-auto">Data yang anda cari kosong !</p>
-            @endforelse
-          </div>
-        </div>
-      </div>
-@endsection
-=======
     <!-- Header Start -->
     <div class="container-fluid header bg-white p-0">
         <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
@@ -85,8 +13,8 @@
                     </ol>
                 </nav>
             </div>
-            <div class="col-md-6 animated fadeIn">
-                <img class="img-fluid" style="width: 50%; align-items:center" src="{{ asset('frontend/images/carousel/1fortuner.jpg') }}" alt="">
+            <div class="col-md-6 wow slideInRight" data-wow-delay="0.3s" >
+                <img class="img-fluid" style="width: 100%; align-items:center" src="{{ asset('frontend/img/header/header-3.jpg') }}" alt="">
             </div>
             <hr>
         </div>
@@ -262,4 +190,3 @@
         </div>
     </div> --}}
 @endsection
->>>>>>> origin/branch_NidaAuliaKarima
--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -49,7 +49,7 @@
         <!-- Navbar Start -->
         <div class="container-fluid nav-bar bg-transparent">
             <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
-                <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
+                <a href="{{ url('/') }}" class="navbar-brand d-flex align-items-center text-center">
                     <div class="icon p-2 me-2">
                         <img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png" alt="Icon" style="width: 50px; height: 50px;">
                         {{-- <h1>DeMobil</h1> --}}
