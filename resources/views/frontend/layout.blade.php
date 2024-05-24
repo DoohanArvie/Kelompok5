e3eaeb8b0e02f570d29ad42fbf5e4f4370d21c64
valentinoAldo <valentinoaldoo7@gmail.com>
2024-05-24 09:07:36 +0700

    tampilan profile:verivy email

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -103,7 +103,7 @@
                           {{ Auth::user()->name }}
                           </a>
                           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <a class="dropdown-item" href="#">
+                              <a class="dropdown-item" href="{{ route('profile.index') }}">
                                   Profile
                               </a>
                               <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Frontend\AboutController;
 use App\Http\Controllers\Frontend\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,30 +32,29 @@ Route::get('tentang-kami', [AboutController::class, 'index']);
 Route::get('kontak', [ContactController::class, 'index']);
 Route::post('kontak', [ContactController::class, 'store'])->name('contact.store');
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
+});
+
 Route::get('password/reset', 'App\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'App\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'App\Http\Controllers\Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'App\Http\Controllers\Auth\ResetPasswordController@reset')->name('password.update');
 
-
-
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('is_admin');
-//Route::get('/home', [HomeController::class, 'index'])->name('home');
 
+Route::group(['middleware' => ['is_admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('users', [UserController::class, 'index'])->name('users.index');
 
-    Route::group(['middleware' => ['is_admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-        Route::get('users', [UserController::class, 'index'])->name('users.index');
-
-        Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
-        Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::resource('cars', \App\Http\Controllers\Admin\CarController::class);
-        Route::resource('types', \App\Http\Controllers\Admin\TypeController::class);
-        Route::resource('testimonials', \App\Http\Controllers\Admin\TestimonialController::class);
-        Route::resource('teams', \App\Http\Controllers\Admin\TeamController::class);
-        Route::resource('settings', \App\Http\Controllers\Admin\SettingController::class)->only(['index','store','update']);
-        Route::resource('contacts', \App\Http\Controllers\Admin\ContactController::class)->only(['index','destroy']);
-        Route::resource('bookings', \App\Http\Controllers\Admin\BookingController::class)->only(['index','destroy']);
-        Route::resource('blogs', \App\Http\Controllers\Admin\BlogController::class);
-    });
+    Route::resource('cars', \App\Http\Controllers\Admin\CarController::class);
+    Route::resource('types', \App\Http\Controllers\Admin\TypeController::class);
+    Route::resource('testimonials', \App\Http\Controllers\Admin\TestimonialController::class);
+    Route::resource('teams', \App\Http\Controllers\Admin\TeamController::class);
+    Route::resource('settings', \App\Http\Controllers\Admin\SettingController::class)->only(['index','store','update']);
+    Route::resource('contacts', \App\Http\Controllers\Admin\ContactController::class)->only(['index','destroy']);
+    Route::resource('bookings', \App\Http\Controllers\Admin\BookingController::class)->only(['index','destroy']);
+    Route::resource('blogs', \App\Http\Controllers\Admin\BlogController::class);
+});
